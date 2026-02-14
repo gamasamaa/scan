34e7159d4a04b82160fb90805cca1bcd37901b65
gama <agamalielreyes@gmail.com>
2026-02-13 23:48:36 -0400

    webo

--- scanner.py
+++ scanner.py
@@ -10,7 +10,3 @@ c = conn.cursor()
 print(datetime.time.hour)
 # while True:
 #     dbworker.studentSearch(c)
-# otro cambio a lo loco pa ver como brega
-# hacer cambios aca para bregar con el pull
-# vamos a ver mas cambios webo webo webo
-# quesadilla 34,000 veces por puta
